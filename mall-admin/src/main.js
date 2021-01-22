2febce9c0499c5fdb3df791f76de3332a16bdada
HaruTheKing <ziyiliu525@gmail.com>
2021-01-22 11:43:51 +0800

    router bug修复

--- mall-admin/src/main.js
+++ mall-admin/src/main.js
@@ -5,6 +5,11 @@ import router from "./router";
 import store from "./store";
 import "ant-design-vue/dist/antd.css";
 import VueRouter from "vue-router";
+import Router from "vue-router";
+const routerPush = Router.prototype.push;
+Router.prototype.push = function push(location) {
+  return routerPush.call(this, location).catch((error) => error);
+};
 Vue.use(VueRouter);
 Vue.config.productionTip = false;
 Vue.use(Antd);
