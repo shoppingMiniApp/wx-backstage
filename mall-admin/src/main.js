207f3b650d840c57f4f30403ad30cb9a4a2ff4fa
HaruTheKing <ziyiliu525@gmail.com>
2021-01-22 11:00:54 +0800

    框架bug修复

--- mall-admin/src/main.js
+++ mall-admin/src/main.js
@@ -1,9 +1,11 @@
-import Vue from "vue";
+import Vue from "vue/dist/vue.js";
 import Antd from "ant-design-vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "ant-design-vue/dist/antd.css";
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
 Vue.config.productionTip = false;
 Vue.use(Antd);
 
