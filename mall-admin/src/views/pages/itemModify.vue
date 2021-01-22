fabc6c796f3b490380599572b9ff2962fb8a495f
xiehaoyu1 <xiehaoyu1@126.com>
2021-01-22 14:40:39 +0800

    对比对比

--- mall-admin/src/views/pages/itemModify.vue
+++ mall-admin/src/views/pages/itemModify.vue
@@ -1,12 +1,256 @@
-// 物品修改
+//* 物品修改
 <template>
-  <div>物品修改</div>
+  <div>
+    <div>
+      <a-button class="editable-add-btn" @click="handleAdd"> Add </a-button>
+
+      <a-table
+        bordered
+        :data-source="dataSource"
+        :columns="columns"
+        :row-selection="rowSelection"
+      >
+        <a-table
+          :row-selection="rowSelection"
+          :columns="columns"
+          :data-source="data"
+        />
+        <template slot="name" slot-scope="text, record">
+          <editable-cell
+            :text="text"
+            @change="onCellChange(record.key, 'name', $event)"
+          />
+        </template>
+        <template slot="operation" slot-scope="text, record">
+          <a-popconfirm
+            v-if="dataSource.length"
+            title="Sure to delete?"
+            @confirm="() => onDelete(record.key)"
+          >
+            <a href="javascript:;">Delete</a>
+          </a-popconfirm>
+        </template>
+      </a-table>
+    </div>
+  </div>
 </template>
 <script>
-export default {
+const data = [];
+for (let i = 0; i < 46; i++) {
+  data.push({
+    key: i,
+    name: `Edward King ${i}`,
+    age: 32,
+    address: `London, Park Lane no. ${i}`,
+  });
+}
+
+const EditableCell = {
+  template: `
+      <div class="editable-cell">
+        <div v-if="editable" class="editable-cell-input-wrapper">
+          <a-input :value="value" @change="handleChange" @pressEnter="check" /><a-icon
+            type="check"
+            class="editable-cell-icon-check"
+            @click="check"
+          />
+        </div>
+        <div v-else class="editable-cell-text-wrapper">
+          {{ value || ' ' }}
+          <a-icon type="edit" class="editable-cell-icon" @click="edit" />
+        </div>
+      </div>
+    `,
+  props: {
+    text: String,
+  },
   data() {
-    return {};
-  }
+    return {
+      value: this.text,
+      editable: false,
+    };
+  },
+  methods: {
+    handleChange(e) {
+      const value = e.target.value;
+      this.value = value;
+    },
+    check() {
+      this.editable = false;
+      this.$emit("change", this.value);
+    },
+    edit() {
+      this.editable = true;
+    },
+  },
+};
+export default {
+  components: {
+    EditableCell,
+  },
+  data() {
+    return {
+      data,
+      selectedRowKeys: [], // Check here to configure the default column
+      dataSource: [
+        {
+          key: 0,
+          name: "Edward King 0",
+          age: "32",
+          address: "London, Park Lane no. 0",
+        },
+        {
+          key: 1,
+          name: "Edward King 1",
+          age: "32",
+          address: "London, Park Lane no. 1",
+        },
+      ],
+      count: 2,
+      columns: [
+        {
+          title: "name",
+          dataIndex: "name",
+          width: "30%",
+          scopedSlots: { customRender: "name" },
+        },
+        {
+          title: "age",
+          dataIndex: "age",
+        },
+        {
+          title: "address",
+          dataIndex: "address",
+        },
+        {
+          title: "operation",
+          dataIndex: "operation",
+          scopedSlots: { customRender: "operation" },
+        },
+      ],
+    };
+  },
+  computed: {
+    rowSelection() {
+      const { selectedRowKeys } = this;
+      return {
+        selectedRowKeys,
+        onChange: this.onSelectChange,
+        hideDefaultSelections: true,
+        selections: [
+          {
+            key: "all-data",
+            text: "Select All Data",
+            onSelect: () => {
+              this.selectedRowKeys = [...Array(46).keys()]; // 0...45
+            },
+          },
+          {
+            key: "odd",
+            text: "Select Odd Row",
+            onSelect: (changableRowKeys) => {
+              let newSelectedRowKeys = [];
+              newSelectedRowKeys = changableRowKeys.filter((key, index) => {
+                if (index % 2 !== 0) {
+                  return false;
+                }
+                return true;
+              });
+              this.selectedRowKeys = newSelectedRowKeys;
+            },
+          },
+          {
+            key: "even",
+            text: "Select Even Row",
+            onSelect: (changableRowKeys) => {
+              let newSelectedRowKeys = [];
+              newSelectedRowKeys = changableRowKeys.filter((key, index) => {
+                if (index % 2 !== 0) {
+                  return true;
+                }
+                return false;
+              });
+              this.selectedRowKeys = newSelectedRowKeys;
+            },
+          },
+        ],
+        onSelection: this.onSelection,
+      };
+    },
+  },
+  methods: {
+    onSelectChange(selectedRowKeys) {
+      console.log("selectedRowKeys changed: ", selectedRowKeys);
+      this.selectedRowKeys = selectedRowKeys;
+    },
+    onCellChange(key, dataIndex, value) {
+      const dataSource = [...this.dataSource];
+      const target = dataSource.find((item) => item.key === key);
+      if (target) {
+        target[dataIndex] = value;
+        this.dataSource = dataSource;
+      }
+    },
+    // *删除商品
+    onDelete(key) {
+      const dataSource = [...this.dataSource];
+      this.dataSource = dataSource.filter((item) => item.key !== key);
+    },
+    // *添加商品
+    handleAdd() {
+      const { count, dataSource } = this;
+      const newData = {
+        key: count,
+        name: `Edward King ${count}`,
+        age: 32,
+        address: `London, Park Lane no. ${count}`,
+      };
+      this.dataSource = [...dataSource, newData];
+      this.count = count + 1;
+    },
+  },
 };
 </script>
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.editable-cell {
+  position: relative;
+}
+.editable-cell-input-wrapper,
+.editable-cell-text-wrapper {
+  padding-right: 24px;
+}
+.editable-cell-text-wrapper {
+  padding: 5px 24px 5px 5px;
+}
+.editable-cell-icon,
+.editable-cell-icon-check {
+  position: absolute;
+  right: 0;
+  width: 20px;
+  cursor: pointer;
+}
+.editable-cell-icon {
+  line-height: 18px;
+  display: none;
+}
+.editable-cell-icon-check {
+  line-height: 28px;
+}
+.editable-cell:hover .editable-cell-icon {
+  display: inline-block;
+}
+.editable-cell-icon:hover,
+.editable-cell-icon-check:hover {
+  color: #108ee9;
+}
+.editable-add-btn {
+  margin-bottom: 8px;
+}
+/deep/.editable-cell-input-wrapper {
+  display: flex;
+  .editable-cell-icon-check {
+    padding-top: 8px;
+    padding-left: 5px;
+  }
+}
+</style>
